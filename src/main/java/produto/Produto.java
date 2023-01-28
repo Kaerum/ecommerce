145e31c60bdde977b80cf7fc593253832354e0ed
Gabriel Porto <gsporto226@gmail.com>
2023-01-27 21:55:03 -0300

    Commit inicial

---
+++ src/main/java/produto/Produto.java
@@ -0,0 +1,8 @@
+package produto;
+
+public class Produto {
+    String nome;
+    double preco;
+    String marca;
+
+}
---
+++ src/main/java/usuarios/Administrador.java
@@ -0,0 +1,10 @@
+package usuarios;
+
+public class Administrador {
+    String CNPJ;
+
+    // cadastrar produto
+    // visualizar lista de produtos
+    // editar produto x
+    // remover produto x
+}
