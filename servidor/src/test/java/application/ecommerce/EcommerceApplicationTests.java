8c7c67072c6b5c8d939a9db84c1e479cbb5d004b
Gabriel Porto <gsporto226@gmail.com>
2023-03-06 15:20:24 -0300

    Inicial

--- servidor/src/test/java/application/ecommerce/EcommerceApplicationTests.java
+++ servidor/src/test/java/application/ecommerce/EcommerceApplicationTests.java
@@ -6,8 +6,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class EcommerceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	// Apesar de não tomar tanto tempo assim para fazer testes, resolvi deixar de lado
+	// considerando que o próximo módulo será sobre isso.
 
 }
